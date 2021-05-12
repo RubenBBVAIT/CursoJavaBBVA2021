3f6cd599b009df2f9735d87672ab728bac86a978
Ruben <ruben.santana.colino@bbvaitspain.com>
2021-05-12 12:57:46 +0200

    sql updates

---
+++ CursoJava2021/src/modelo/sql/updates.sql
@@ -0,0 +1,5 @@
+UPDATE figuras SET FIG_NOMBRE = 'cuadrado' WHERE FIG_TIPO = 1;
+UPDATE figuras SET FIG_NOMBRE = 'circulo' WHERE FIG_TIPO = 2;
+UPDATE figuras SET FIG_NOMBRE = 'rectangulo' WHERE FIG_TIPO = 3;
+UPDATE figuras SET FIG_NOMBRE = 'triangulo' WHERE FIG_TIPO = 4;
+UPDATE figuras SET FIG_NOMBRE = 'poligono' WHERE FIG_TIPO = 5;
